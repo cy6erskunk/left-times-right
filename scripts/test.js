c368a764a55355af0ff379581667e98d8d78b652
Igor <cyberskunk@gmail.com>
2025-07-22 19:32:29 +0000

    Fix test runner script

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,5 @@
 'use strict'
-import jest from 'jest'
+import {run} from 'jest'
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test'
@@ -27,4 +27,4 @@ if (
   argv.push('--watch')
 }
 
-jest.run(argv)
+run(argv)
